c30068b56b18c64ddeb81e0b35a31e7f2a30777a
Rommel Santos <rommelrenne@gmail.com>
2021-08-05 21:06:37 -0300

    Fixed header sticky bug

--- main.js
+++ main.js
@@ -18,20 +18,14 @@ function toggleTypes() {
 
 window.addEventListener("load", toggleTypes);
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the header
-var header = document.getElementById("myHeader");
-
-// Get the offset position of the navbar
+var header = document.getElementById("top-header");
 var sticky = header.offsetTop;
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+window.onscroll = () => {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
   }
-}
+};
+
